270e3e745cf7af7b1a95f016ace6e9802e9114ee
rrfunde12 <happyhearts@outlook.in>
2015-06-04 20:44:33 +0530

    Create duplicate_remove.py

---
+++ duplicate_remove.py
@@ -0,0 +1,45 @@
+import os
+import sys
+dir1=sys.argv[1]
+os.chdir(dir1)
+os.system("find -not -empty -type f -printf \"%s\n\" | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 md5sum |  uniq -w32 --all-repeated=separate>files.txt")
+
+
+f=open("files.txt","r")
+dup=open("dup","w")
+cwd=os.getcwd()
+while(True):
+	data=f.readline()
+	index=data.find("/")
+	if(index!=-1):
+		dup.write(cwd+data[index:-1]+" ")
+	else:
+		dup.write(data)
+	if(data.find("\n")==-1):
+		break
+dup.close()				
+dup=open("dup","r")
+i=0
+while True:
+	data=dup.readline()
+	if(data==""):
+		break
+	i+=1
+	temp=data.split(" ")
+	sys.stdout.write(str(i))
+	for j in temp:
+		print "\t"+j
+		
+ch=raw_input("choose option to delete (press n to stop)")		
+if(ch=="n"):
+	exit()
+dup.seek(0)	
+
+for i,line in enumerate(dup):
+	if(i==int(ch)-1):
+		temp=line.split(" ")	
+		for i in range(len(temp)):
+			print str(i+1)+"\t"+temp[i]
+		c=input()
+		os.remove(temp[c-1])
+		break	
